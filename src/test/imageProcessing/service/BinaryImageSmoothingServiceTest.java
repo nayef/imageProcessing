d75a391674bb9704829d116fe17ff568cb340399
nayef <nayef.reza@gmail.com>
2015-01-27 18:34:32 +0600

    Added service for smoothing binary images

---
+++ src/test/imageProcessing/service/BinaryImageSmoothingServiceTest.java
@@ -0,0 +1,41 @@
+package test.imageProcessing.service;
+
+import imageProcessing.service.BinaryImageSmoothingService;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinaryImageSmoothingServiceTest {
+
+    BinaryImageSmoothingService thinningService;
+
+    @Before
+    public void setUp() throws Exception {
+        thinningService = new BinaryImageSmoothingService();
+    }
+
+    @Test
+    public void testSmoothBinaryImage() throws Exception {
+
+        int[][] inputData = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 1},
+                {0, 1, 0, 0, 0, 1, 1},
+                {1, 0, 1, 0, 1, 1, 1},
+                {1, 1, 1, 0, 0, 0, 0}
+        };
+
+        int[][] resultData = thinningService.smoothBinaryImage(inputData);
+
+        int[][] expectedResult = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1},
+                {0, 1, 0, 0, 1, 1, 1},
+                {1, 1, 1, 0, 1, 1, 1},
+                {1, 1, 1, 0, 0, 0, 0}
+        };
+
+        assertArrayEquals(expectedResult, resultData);
+    }
+}
